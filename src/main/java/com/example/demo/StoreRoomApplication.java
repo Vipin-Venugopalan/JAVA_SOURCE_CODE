5949931a7b6387af720e1fb80520dc9640dc1378
vipinvenugopalan <vipinvenugopalan03@gmail.com>
2020-10-24 01:54:31 -0700

    first commit

---
+++ src/main/java/com/example/demo/StoreRoomApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreRoomApplication.class, args);
+	}
+
+}
